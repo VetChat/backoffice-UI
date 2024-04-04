71486614aee335e6d47b20d21fdb8891dc2a582e
abounk <pakapop.bounk@gmail.com>
2024-04-05 00:08:03 +0700

    feat: Install deps

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
